836876f8085619eb23de31477b3a95d554712e0a
sai-sourav <saisourav123@gmail.com>
2022-11-17 22:55:43 +0530

    added res.write/setheader ,req.url/method/headers

--- app.js
+++ app.js
@@ -15,7 +15,29 @@ const http = require('http');
 // or
 
 const server = http.createServer((req,res) => {
-     console.log("sourav");
+    //  console.log("sourav");
+    //  process.exit();
+    console.log(req.headers);
+    console.log("///////////////////////////");
+    console.log(req.url);
+    console.log(req.method);
+    console.log("-end-");
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> my first page </title></head>');
+    if (req.url === "/home"){
+        res.write('<body><h1>Welcome Home</h1></body>')
+    }
+    else if (req.url === "/about"){
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+    }
+    else if (req.url === "/node"){
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    }else{
+        res.write('<body><h1>Hello from my Node Js server!</h1></body>')
+    }
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(4000);
