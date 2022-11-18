d6f2cfc6104205ca57d24ad225046a851ffc7a5e
sai-sourav <saisourav123@gmail.com>
2022-11-18 07:45:00 +0530

    used on()listener with data, end

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
 const http = require('http');
-
-// function rqListener(req,res){
-
-// }
-
-// http.createServer(rqListener);
-
-// or
-
-// http.createServer(function(req,res){
-
-// });
-
-// or
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     //  console.log("sourav");
     //  process.exit();
-    console.log(req.headers);
-    console.log("///////////////////////////");
-    console.log(req.url);
-    console.log(req.method);
-    console.log("-end-");
+    // console.log(req.headers);
+    // console.log("///////////////////////////");
+    // console.log(req.url);
+    // console.log(req.method);
+    // console.log("-end-");
+    const method = req.method;
+    if(req.url === "/"){
+        res.write('<html>');
+        res.write('<head><title> Enter message </title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>send</button></body>')
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (req.url === "/message" && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        }) //new chunk ready to be read
+        return req.on('end', () => {
+           const parsedBody = Buffer.concat(body).toString();
+           const message = parsedBody.split('=')[1];
+        //    fs.writeFile('message.txt',message, err => {
+        //         res.statusCode = 302;
+        //         res.setHeader('Location','/');
+        //         return res.end();
+        //    });
+           res.write(`<body><form action="/message" method="POST"><p>${message}</p><input type="text" name="message"><button>send</button></body>`);
+           return res.end();
+        })   
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title> my first page </title></head>');
-    if (req.url === "/home"){
-        res.write('<body><h1>Welcome Home</h1></body>')
-    }
-    else if (req.url === "/about"){
-        res.write('<body><h1>Welcome to About Us page</h1></body>')
-    }
-    else if (req.url === "/node"){
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-    }else{
-        res.write('<body><h1>Hello from my Node Js server!</h1></body>')
-    }
+    res.write('<body><h1>Welcome to my Node Js project message page</h1></body>');
     res.write('</html>');
     res.end();
 });
