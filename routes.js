600a61a9ee6b1ea0303d36074632e4fd61398c41
sai-sourav <saisourav123@gmail.com>
2022-11-18 21:04:50 +0530

    write and read file configured

--- routes.js
+++ routes.js
@@ -1,39 +1,42 @@
-// const fs = require('fs');
+const fs = require('fs');
 const routeserver = (req,res) => {
     const method = req.method;
     if(req.url === "/"){
-        res.write('<html>');
-        res.write('<head><title> Enter message </title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>send</button></body>')
-        res.write('</html>');
-        return res.end();
+        fs.readFile('message.txt',(err,data) => {
+            if (err){
+                console.log(err);
+            }
+            res.write('<html>');
+            res.write('<head><title> Enter message </title></head>');
+            res.write(`<body><form action="/message" method="POST"><p>${data}</p><input type="text" name="message"><button>send</button></body>`)
+            res.write('</html>');
+            return res.end();
+        })
+        
     }
 
-    if (req.url === "/message" && method === 'POST'){
+    else if (req.url === "/message" && method === 'POST'){
         const body = [];
         req.on('data', (chunk) =>{    //new chunk ready to be read
             body.push(chunk);
         })
         return req.on('end', () => {  // no chunk is available to read
            const parsedBody = Buffer.concat(body).toString();
-           const message = parsedBody.split('=')[1].replace('+',' ');
-           fs.writeFile('message.txt',message, err => {
+           const message = parsedBody.split('=')[1];
+           fs.writeFile('message.txt'," "+message, err => {
                 res.statusCode = 302;
                 res.setHeader('Location','/');
                 return res.end();
            });
-        //    res.setHeader('Location','/');
-        //    res.write(`<body><form action="/message" method="POST"><p>${message}</p><input type="text" name="message"><button>send</button></body>`);
-        //    return res.end();
         })   
     }
     // fast forward code
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> my first page </title></head>');
-    res.write('<body><h1>Welcome to my Node Js project message page</h1></body>');
-    res.write('</html>');
-    res.end();
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<html>');
+    // res.write('<head><title> my first page </title></head>');
+    // res.write('<body><h1>Welcome to my Node Js project message page</h1></body>');
+    // res.write('</html>');
+    // res.end();
 }
 
 // module.exports = routeserver;
