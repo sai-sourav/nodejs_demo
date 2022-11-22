b3fd9ee476f7fc1382d168c6bac6a121d2a85ee6
sai-sourav <saisourav123@gmail.com>
2022-11-22 10:24:34 +0530

    used express middlewares with use and listen

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const http = require('http');
-const routes = require('./routes');
-const server = http.createServer(routes.handler);
-server.listen(4000);
+// const http = require('http');
+// const routes = require('./routes');
+const express = require('express');  //here express returns a 'e' named fuction
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    //....
+    res.send('{ key1: value }')
+})
+// const server = http.createServer(app);
+// server.listen(4000);
+app.listen(3000);
