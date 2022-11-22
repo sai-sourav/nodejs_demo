95b82e0eafb9c8e9ce850ceaf5a366e9be3a779b
sai-sourav <saisourav123@gmail.com>
2022-11-22 11:18:31 +0530

    added bodyparses used app.post

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 // const http = require('http');
 // const routes = require('./routes');
+const bodyparser = require('body-parser');
 const express = require('express');  //here express returns a 'e' named fuction
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="text" name="size"><br><button type="submit">Add product</button>')
+    
 })
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    //....
-    res.send('{ key1: value }')
+// app.use('/product',(req, res, next) => {
+//     console.log(req.body.title);
+//     res.redirect('/add-product');
+// })
+
+app.post('/product',(req, res, next) => {
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/add-product');
+})
+
+app.use('/',(req, res, next) => {
+    res.send('<h1>hello from express.js</h1>')
 })
 // const server = http.createServer(app);
 // server.listen(4000);
